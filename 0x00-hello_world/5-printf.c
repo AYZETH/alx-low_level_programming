4b428b0c78ea3c4f730cd26ae8e60f1b893d8a61
abdeslam <abdeslamtouahe@gmail.com>
2023-08-10 17:50:41 -0700

    C program prints exactly a specific string using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - the main function
+ *return: always return 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return(0); 
+}
